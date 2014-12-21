267a5aed6e4b964304fba6bd440a8955655c9565
Timur Bilalov <t.bilalov@hh.ru>
2014-12-21 19:37:29 +0300

    axiomlib add sh

---
+++ generate_axiomlib.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+OUT=$1
+
+if !("$OUT")
+then 
+	OUT="dataset"
+fi
+
+if !([ -d "$OUT" ])
+then 
+	mkdir "$OUT" 
+fi
+
+python generate.py $OUT/reference
+python generate.py $OUT/testing
+
+
+cp extra/class_names $OUT/class_names
+cp extra/param_names $OUT/param_names
+cp extra/dataset.conf $OUT/$OUT.conf
